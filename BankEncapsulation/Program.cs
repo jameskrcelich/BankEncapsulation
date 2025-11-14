f13b06ca2c341a3b40e46c7a9624d256a0b1e2f4
James Krcelich <james.krcelich@sbcglobal.net>
2025-11-13 18:18:31 -0600

    final update

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -8,14 +8,24 @@ namespace BankEncapsulation
         static void Main(string[] args)
         {
             var account = new BankAcccount();
-            
-            //account.balance = 300;
-            Console.WriteLine("How much would you like to deposit?");
-            var deposit = double.Parse(Console.ReadLine());
-            
-            account.Deposit(deposit);
-            
-            Console.WriteLine($"Your new balance = {account.GetBalance()}");
+            double deposit = 0;
+
+            do
+            {
+                Console.WriteLine("How much would you like to deposit? (Enter 0 to exit)");
+                deposit = double.Parse(Console.ReadLine());
+
+                if (deposit <= 0)
+                {
+                    break;
+                }
+
+                account.Deposit(deposit);
+                Console.WriteLine($"You successfully deposited ${deposit}");
+                Console.WriteLine($"Your new balance = {account.GetBalance()}");
+
+            } while (deposit >= 0);
+
         }
     }
 }
